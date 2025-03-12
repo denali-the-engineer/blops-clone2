fb0370dffa07ce20c043df154c5612742d9e09db
denali-the-engineer <neodenali@gmail.com>
2025-03-12 10:03:39 -0400

    Create bottomScroller.jsx

---
+++ src/components/bottomScroller.jsx
@@ -0,0 +1,127 @@
+import { DisplaySettings } from "@mui/icons-material";
+import "../app/fadeinAnim.css"
+import { Grid2 } from "@mui/material";
+
+export default function BottomScroller() {
+
+    return (
+        <div className="fadeInClass" style={{position: "fixed", zIndex: 1000000000000, height: 40, width: "200vw", left: 0, bottom: 0, background: "linear-gradient(360deg, rgba(0,0,0,1) 25%, rgba(0,0,0,0.1) 100%)"}}>
+            <Grid2 style={{position: "absolute"}} className="scrollingAnimationClass" container>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+            </Grid2>
+            
+            <Grid2 style={{position: "absolute"}} className="scrollingAnimationClass2" container>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+            </Grid2>
+
+            <Grid2 style={{position: "absolute"}} className="scrollingAnimationClass3" container>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+                <Grid2 style={{height: 40, color: "white", width: 300, display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <img style={{borderRadius: "50%", height: 25, width: 25}} src="https://dd.dexscreener.com/ds-data/tokens/solana/Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs.png?key=f4d065"/>
+                    <div style={{marginLeft: 10, fontSize: 14}}>GRASS</div>
+                    <div style={{marginLeft: 10, fontSize: 14}}>$0.039</div>
+                    <div style={{marginLeft: 10, fontSize: 14, color: "lime"}}>+10.13%</div>
+                </Grid2>
+            </Grid2>
+        </div>
+    );
+}
